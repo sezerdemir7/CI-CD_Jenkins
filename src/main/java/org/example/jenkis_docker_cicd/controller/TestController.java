996925428e573ba5f2de3acc11b5096e325a82f0
sezerd <99814610+sezerdemir7@users.noreply.github.com>
2024-04-30 19:24:41 +0300

    updated

--- src/main/java/org/example/jenkis_docker_cicd/controller/TestController.java
+++ src/main/java/org/example/jenkis_docker_cicd/controller/TestController.java
@@ -15,6 +15,6 @@ public class TestController {
     }
     @GetMapping("m")
     public String helloWorld2(){
-        return  "Hello world hello merhaba";
+        return  "Hello world hello eray";
     }
 }
